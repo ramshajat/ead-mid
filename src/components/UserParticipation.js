59afcaa76cb2e8e8e94c9f33282284dbbbb955b7
Ramsha Jat <ramsha.bscsf19@iba-suk.edu.pk>
2023-05-27 17:09:33 +0500

    Adduseparticipation

---
+++ src/components/UserParticipation.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import PollDisplayComponent from './PollDisplayComponent'
+function UserParticipation() {
+//  const[count,setcount]=useState(0);
+//  useEffect(() => {
+//     setcount: `${count}`
+// }, [count])
+// const handleChange = (choice) => {
+//     choice === selItem ? setSelItem(null) : setSelItem( choice);
+//   };
+  return (
+    <div>
+        <PollDisplayComponent></PollDisplayComponent>
+        <div>{choice}</div>
+    </div>
+  )
+}
+const choices =[
+    { "id": 1, "label": "JavaScript", "votes": 0 },
+
+    { "id": 2, "label": "Python", "votes": 5 },
+
+    { "id": 3, "label": "Java", "votes": 7},
+
+    { "id": 4, "label": "C#", "votes": 0 }]
+const choice =choices.map(choice=>(<div>{choice.label} got {choice.votes}</div>))
+
+
+export default UserParticipation
